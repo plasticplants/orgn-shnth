5e0df8efa4b7bbd0494baeda32fb8ae8aab67a4d
Austin <austinrowangrove@gmail.com>
2022-10-22 12:51:55 -0500

    Update shnth.lua

--- lib/shnth.lua
+++ lib/shnth.lua
@@ -59,10 +59,23 @@ function shnth.event(typ, code, val)
 end
 
 function shnth.bar(n, d)
-  -- bar input, always firing
-  -- n bar index
-  -- d bar depth
+  orgn.noteOn(61, mu.note_num_to_freq(61), ((d or 127/127)*0.2 + 0.85)
 end
+
+
+-- local m = midi.connect()
+-- m.event = function(data)
+--     local msg = midi.to_msg(data)
+
+--     if msg.type == "note_on" then
+--         --TODO: velocity range params
+--         orgn.noteOn(msg.note, mu.note_num_to_freq(msg.note), ((msg.vel or 127)/127)*0.2 + 0.85)
+--     elseif msg.type == "note_off" then
+--         orgn.noteOff(msg.note)
+--     end
+-- end
+
+-- return m
 
 function shnth.corp(n, d)
   -- corp input, always firing
